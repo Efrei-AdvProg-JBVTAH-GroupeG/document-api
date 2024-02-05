522b90a8f6be2759ad908ae9b8d9da4453c9860e
thibault <thibault.henrion@gmail.com>
2024-02-05 10:03:22 +0100

    fix cors

--- src/main/java/fr/efrei/documentapi/configuration/WebSecurityConfig.java
+++ src/main/java/fr/efrei/documentapi/configuration/WebSecurityConfig.java
@@ -73,8 +73,10 @@ public class WebSecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("*"));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST"));
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token"));
+        configuration.setExposedHeaders(Arrays.asList("x-auth-token"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
